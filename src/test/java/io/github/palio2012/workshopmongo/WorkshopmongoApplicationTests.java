bc0fc8dddd32eac9da84591f63f2a8766f319896
Palio2012 <daniasrayan.com@gmail.com>
2022-11-16 15:24:29 -0300

    Project created

---
+++ src/test/java/io/github/palio2012/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.palio2012.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
