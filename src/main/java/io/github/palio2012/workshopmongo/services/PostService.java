8b3de449d4939b09eecdbbe0059e929a9db2890c
Palio2012 <daniasrayan.com@gmail.com>
2022-11-20 20:33:36 -0300

    Post findById

---
+++ src/main/java/io/github/palio2012/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package io.github.palio2012.workshopmongo.services;
+
+import io.github.palio2012.workshopmongo.domain.Post;
+import io.github.palio2012.workshopmongo.repository.PostRepository;
+import io.github.palio2012.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public List<Post> findAll() {
+        return repo.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
+
