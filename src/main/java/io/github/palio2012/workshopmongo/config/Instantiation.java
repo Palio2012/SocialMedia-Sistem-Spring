2138d06f8129e0a686fe552fffff859160047282
Palio2012 <daniasrayan.com@gmail.com>
2022-11-20 19:18:51 -0300

    AuthorDTO implemented

--- src/main/java/io/github/palio2012/workshopmongo/config/Instantiation.java
+++ src/main/java/io/github/palio2012/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package io.github.palio2012.workshopmongo.config;
 
 import io.github.palio2012.workshopmongo.domain.Post;
 import io.github.palio2012.workshopmongo.domain.User;
+import io.github.palio2012.workshopmongo.dto.AuthorDTO;
 import io.github.palio2012.workshopmongo.repository.PostRepository;
 import io.github.palio2012.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
