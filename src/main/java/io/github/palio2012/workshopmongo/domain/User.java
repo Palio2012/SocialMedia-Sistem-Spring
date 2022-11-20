df308b28604747d8abd2c9cc79f92b74405a84cb
Palio2012 <daniasrayan.com@gmail.com>
2022-11-20 19:30:23 -0300

    User's posts reference

--- src/main/java/io/github/palio2012/workshopmongo/domain/User.java
+++ src/main/java/io/github/palio2012/workshopmongo/domain/User.java
@@ -2,9 +2,12 @@ package io.github.palio2012.workshopmongo.domain;
 
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document (collection = "user")
@@ -16,6 +19,10 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef (lazy = true)
+    private List <Post> posts = new ArrayList<>();
+
 
     public User () {
     }
@@ -48,6 +55,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
