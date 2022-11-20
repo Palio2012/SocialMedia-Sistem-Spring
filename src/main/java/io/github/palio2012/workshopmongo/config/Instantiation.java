7bc1920675e7cbb1e0ceca10e3a5fb101d0d4674
Palio2012 <daniasrayan.com@gmail.com>
2022-11-20 20:44:17 -0300

    Adding comments to Post

--- src/main/java/io/github/palio2012/workshopmongo/config/Instantiation.java
+++ src/main/java/io/github/palio2012/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package io.github.palio2012.workshopmongo.config;
 import io.github.palio2012.workshopmongo.domain.Post;
 import io.github.palio2012.workshopmongo.domain.User;
 import io.github.palio2012.workshopmongo.dto.AuthorDTO;
+import io.github.palio2012.workshopmongo.dto.CommentDTO;
 import io.github.palio2012.workshopmongo.repository.PostRepository;
 import io.github.palio2012.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/io/github/palio2012/workshopmongo/domain/Post.java
+++ src/main/java/io/github/palio2012/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package io.github.palio2012.workshopmongo.domain;
 
 import io.github.palio2012.workshopmongo.dto.AuthorDTO;
+import io.github.palio2012.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -20,6 +23,8 @@ public class Post implements Serializable {
     private String body;
     @ManyToOne
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post () {
     }
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
