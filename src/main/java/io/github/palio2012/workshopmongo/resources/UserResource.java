8759cfa61cfdec9d0c34843c280300fa8e481e15
Palio2012 <daniasrayan.com@gmail.com>
2022-11-20 19:34:32 -0300

    Endpoint to return user's posts

--- src/main/java/io/github/palio2012/workshopmongo/resources/UserResource.java
+++ src/main/java/io/github/palio2012/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package io.github.palio2012.workshopmongo.resources;
 
+import io.github.palio2012.workshopmongo.domain.Post;
 import io.github.palio2012.workshopmongo.domain.User;
 import io.github.palio2012.workshopmongo.dto.UserDTO;
 import io.github.palio2012.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity <List <Post>> findPosts (@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
