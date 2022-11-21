baee5600fbb81153ad985bfb594f9dd8f9603a1f
Palio2012 <daniasrayan.com@gmail.com>
2022-11-20 21:16:58 -0300

    Simple consult with Query annotation (Json pattern)

--- src/main/java/io/github/palio2012/workshopmongo/repository/PostRepository.java
+++ src/main/java/io/github/palio2012/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package io.github.palio2012.workshopmongo.repository;
 
 import io.github.palio2012.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository <Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List <Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase (String text);
 }
